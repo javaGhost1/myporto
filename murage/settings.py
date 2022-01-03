1e2e7d2b17c8a536e5fea451cb5cb9eefe4f9421
javaGhost1 <dennismurage97@gmail.com>
2022-01-03 15:48:59 +0300

    Added custom 404 and 500 error pages

--- murage/settings.py
+++ murage/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@!_uezga(g%ix0sk0_sta+)wb^4)olw6*!uhq+p2&sr%b-qfbl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
